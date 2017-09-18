e01059d95354022e954be0076d76d1791e0f4e08
RGero215 <rgero215@gmail.com>
2017-09-18 06:35:52 -0400

    added While You Wait

---
+++ WebFundamentals/Javascript/While You Wait/countdown.js
@@ -0,0 +1,13 @@
+var daysUntilMyBirthday = 60;
+
+while (daysUntilMyBirthday != 0) {
+    daysUntilMyBirthday -= 1;
+    if (daysUntilMyBirthday >= 30) {
+        console.log(daysUntilMyBirthday, "days it's such a long time for my birthday");
+    } else if (daysUntilMyBirthday <= 30 && daysUntilMyBirthday > 5) {
+        console.log(daysUntilMyBirthday, "days left and it's almost my birthday");
+    } else if (daysUntilMyBirthday <= 5) {
+        console.log(daysUntilMyBirthday, "DAYS UNTIL MY BIRTHDAY");
+    } 
+}
+console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪*•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•«");
