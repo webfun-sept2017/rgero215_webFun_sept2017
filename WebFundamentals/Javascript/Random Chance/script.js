c6bf4d207afcd717b3ddab4ebb4d46df7c93b1c7
RGero215 <rgero215@gmail.com>
2017-09-20 07:53:35 -0400

    added Random Chance

---
+++ WebFundamentals/Javascript/Random Chance/script.js
@@ -0,0 +1,58 @@
+function play() {
+    var quarter = prompt("How Much You Want To Bet")
+    var result;
+    var answer;
+    if (isNaN(quarter) || !quarter) {
+        alert("Invalid Number");
+        play();
+    } else {
+        var randomChance = Math.floor(Math.random() * 100);
+        var randomCoins = Math.floor(Math.random() * 50) + 50;
+        var pickNumber = prompt("Pick A Number Between 1 to 100");
+        var winnings = quarter + randomCoins;
+        while (quarter > 0) {
+            console.log("Your pick: " + pickNumber);
+            console.log("Winning Number: " + randomChance);
+            console.log("Possible Winnings: " + randomCoins);
+            if (pickNumber === randomChance) {
+                result = winnings
+                alert("You Pick the Winning Number.  Your Total Winnings:", result);
+                answer = prompt("Would You Like To Play Again? yes/no")
+                if (answer.toLowerCase() == "yes") {
+                    play()
+                } else if (answer.toLowerCase() == "no") {
+                    alert("See You Next Time")
+                } else if (!answer){
+                    alert("See You Next Time")
+                }
+            } else if (pickNumber !== randomChance) {
+                if (isNaN(pickNumber)) {
+                    alert("Invalid number");
+                    pickNumber = prompt("Pick Another Number Between 1 to 100");
+                    randomChance = Math.floor(Math.random() * 100);
+                    randomCoins = Math.floor(Math.random() * 50) + 50;
+                    quarter;
+                    console.log("Your Balance is: " + quarter);
+                } else {
+                    alert("Good Luck Next Time. You pick: "+ pickNumber + " And The Winning Number Is: "+ randomChance);
+                    pickNumber = prompt("Pick Another Number Between 1 to 100");
+                    randomChance = Math.floor(Math.random() * 100);
+                    randomCoins = Math.floor(Math.random() * 50) + 50;
+                    quarter -= 1;
+                    console.log("Your Balance is: " + quarter);
+                }
+            }
+
+        }
+    }
+    answer = prompt("Your Balance Is 0 Would You Like To Play Again? yes/no")
+    if (answer.toLowerCase() == "yes") {
+        play()
+    } else if (answer.toLowerCase() == "no") {
+        alert("See You Next Time");
+    } else if (!answer){
+        alert("See You Next Time");
+    }
+
+}
+play();
