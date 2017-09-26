14ae18459ea5d3f9235cbab9af9c5ddb29aa99c0
RGero215 <rgero215@gmail.com>
2017-09-26 08:59:17 -0400

    added Disappearing Ninja

---
+++ WebFundamentals/Javascript/Disappearing Ninja/script.js
@@ -0,0 +1,10 @@
+$("document").ready(function () {
+    $("img").click(function () {
+        $(this).hide();
+    })
+});
+$("document").ready(function () {
+    $("button").click(function () {
+        location.reload();
+    })
+});
