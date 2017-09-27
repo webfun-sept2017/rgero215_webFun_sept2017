566d534b5f3aa957e75323ba9d53d4eaec9c3610
RGero215 <rgero215@gmail.com>
2017-09-27 19:20:17 -0400

    added Users

---
+++ WebFundamentals/Javascript/Users/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $("#submit").click(function () {
+
+        $("table").append("<tr><td>" + $("input[name='firstName']").val() + "</td><td>"
+            + $("input[name='lastName']").val() + "</td><td>"
+            + $("input[name='email']").val() + "</td><td>"
+            + $("input[name='phone']").val() + "</td></tr>");
+
+        return false;
+
+    })
+})
